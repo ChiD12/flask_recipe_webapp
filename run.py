28958968bde5e3039855bef6c9618d3c9186fbe9
ChiD12 <daniel.chid@gmail.com>
2020-04-17 20:30:44 -0400

    msg

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from os import environ
+from waitress import serve
 from Cooking import app
 
+port = int(environ.get('PORT', 5000))
 
 if __name__ == "__main__":
-    app.run(environ.get('PORT'))
+    #app.run(environ.get('PORT'))
+    serve(app, host = '0.0.0.0', port=port)
